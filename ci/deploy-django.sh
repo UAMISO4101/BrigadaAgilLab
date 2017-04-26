55a57cb503d8b85024798e435981385eb9808973
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-04-26 13:45:09 -0500

    push a ramas de release

--- ci/deploy-django.sh
+++ ci/deploy-django.sh
@@ -3,6 +3,8 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
 git remote add origin-ci https://$GITHUB_PUSH_USER:$GITHUB_API_KEY@github.com/UAMISO4101/BrigadaAgilLab.git
-git checkout -b develop
+git checkout -b develop > /dev/null
 
-git push origin `git subtree push --prefix django`:release-django --force > /dev/null
+git subtree split --prefix django -b release-django > /dev/null
+
+git push -f origin-ci release-django:release-django > /dev/null
