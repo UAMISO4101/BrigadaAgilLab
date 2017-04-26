5388fe655f26166708365b8a724a737db01334f4
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-04-26 16:49:57 -0500

    Integrar asociar experimento a detalle de proyecto

--- ng/src/app/lab/proyecto/proyecto-experimento/proyecto.asociar.exp.component.ts
+++ ng/src/app/lab/proyecto/proyecto-experimento/proyecto.asociar.exp.component.ts
@@ -7,68 +7,68 @@ import {ExperimentoService} from "../../experimento/service/experimento.service"
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
-  selector: "proyecto-experimento",
-  templateUrl: 'proyecto.asociar.exp.component.html',
-  providers: [ProyectoService, ExperimentoService]
+    selector: "proyecto-experimento",
+    templateUrl: 'proyecto.asociar.exp.component.html',
+    providers: [ProyectoService, ExperimentoService]
 })
 export class ProyectoAsociarExpComponent implements OnInit {
-  public experimentos: Experimento[] = [];
-  public experimento: Experimento[] = [];
-  public idProyecto: string;
-  public proyecto: Proyecto[] = [];
-  public show:string;
+    public experimentos: Experimento[] = [];
+    public experimento: Experimento[] = [];
+    public idProyecto: string;
+    public proyecto: Proyecto[] = [];
+    public show: string;
 
 
+    constructor(private _experimentoService: ExperimentoService,
+                private _proyectoService: ProyectoService,
+                route: ActivatedRoute) {
+        this.idProyecto = route.snapshot.params['id'];
+    }
 
-  constructor(private _experimentoService: ExperimentoService,
-              private _proyectoService: ProyectoService,
-              route: ActivatedRoute) {
-    this.idProyecto = route.snapshot.params['id'];
-  }
+    getExperimentos() {
+        this._experimentoService
+            .getExperimentos()
+            .subscribe((experimentos: Experimento[]) =>
+                this.experimentos = experimentos);
+    }
 
-  getExperimentos() {
-    this._experimentoService
-      .getExperimentos()
-      .subscribe((experimentos: Experimento[]) =>
-        this.experimentos = experimentos);
-  }
+    onSelect(item: Experimento[]) {
 
-  onSelect(item: Experimento[]) {
+        this.experimento = item;
+        this.getProtocolos(item);
+        this.show = "true";
+    }
 
-    this.experimento = item;
-    this.getProtocolos(item);
-    this.show="true";
-    window.scroll(0,300);
-  }
+    getProyecto(item) {
+        this._proyectoService
+            .getProyectos()
+            .subscribe((proyectos: Proyecto[]) =>
+                    this.proyecto = JSON.parse(JSON.stringify(proyectos.filter(p => p.id == parseInt(this.idProyecto))
+                        .pop())),
+                error => console.log(error),
+                () => this.addExperimento(item));
+    }
 
-  getProyecto(item) {
-    this._proyectoService
-      .getProyectos()
-      .subscribe((proyectos: Proyecto[]) =>
-          this.proyecto = JSON.parse(JSON.stringify(proyectos.filter(p => p.id == parseInt(this.idProyecto))
-            .pop())),
-        error => console.log(error),
-        () => this.addExperimento(item));
-  }
+    addExperimento(item) {
+        if (!this.proyecto["experimentos"])
+            this.proyecto["experimentos"] = [];
+        this.proyecto["experimentos"]
+            .push(<Experimento>item);
+        this._proyectoService
+            .asociarProyecto(this.proyecto)
+            .subscribe(res => console.log(res));
+    }
 
-  addExperimento(item) {
-    if (!this.proyecto["experimentos"])
-      this.proyecto["experimentos"] = [];
-    this.proyecto["experimentos"]
-      .push(<Experimento>item);
-    this._proyectoService
-      .asociarProyecto(this.proyecto)
-      .subscribe(res => console.log(res));
-  }
-getProtocolos(item){
+    getProtocolos(item) {
 
-   this._experimentoService.getProtocolos(item).subscribe(res=>this.experimento["protocolos"]=res,
-   error=>console.log(error),
-       ()=>console.log(this.experimento));
-}
-  ngOnInit(): any {
-    this.getExperimentos();
-    window.scrollTo(0, 0);
+        this._experimentoService.getProtocolos(item).subscribe(res => this.experimento["protocolos"] = res,
+            error => console.log(error),
+            () => console.log(this.experimento));
+    }
 
-  }
+    ngOnInit(): any {
+        this.getExperimentos();
+        window.scrollTo(0, 0);
+
+    }
 }
