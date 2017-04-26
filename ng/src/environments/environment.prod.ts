0d23b407416c8863f7624a1b12b9e4ec09c4dba0
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-04-26 09:35:44 -0500

    configuracion para heroku

--- ng/src/environments/environment.prod.ts
+++ ng/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
     production: true,
-    url_servicios: process.env.url_servicios
+    url_servicios: process.env.url_servicios || 'http://127.0.0.1:8000/servicios/'
 };
