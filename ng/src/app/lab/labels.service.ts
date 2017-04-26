d2c9045daea8c7084032b445f2cce4168a1d00bd
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-04-25 19:11:41 -0500

    UI - Ajustes de issues generados por eslint

--- ng/src/app/lab/labels.service.ts
+++ ng/src/app/lab/labels.service.ts
@@ -1,31 +1,33 @@
 import {Injectable} from '@angular/core';
 
 export const LABELS = {
-    tit_inicio: "Inicio",
-    tit_acciones: "Acciones",
-    tit_proyecto: "Proyecto",
-    tit_proyectos: "Proyectos",
-    tit_experimento: "Experimento",
-    tit_experimentos: "Experimentos",
-    tit_res_experimento: "Resumen Experimento",
-    tit_protocolo: "Protocolo",
-    tit_protocolos: "Protocolos",
-    tit_nuevo_proyecto: "Nuevo Proyecto",
-    tit_todos_proyectos: "Todos los Proyectos",
-    tit_info_basica: "Información Basica",
+    tit_inicio: 'Inicio',
+    tit_acciones: 'Acciones',
+    tit_proyecto: 'Proyecto',
+    tit_proyectos: 'Proyectos',
+    tit_experimento: 'Experimento',
+    tit_experimentos: 'Experimentos',
+    tit_res_experimento: 'Resumen Experimento',
+    tit_protocolo: 'Protocolo',
+    tit_protocolos: 'Protocolos',
+    tit_nuevo_proyecto: 'Nuevo Proyecto',
+    tit_todos_proyectos: 'Todos los Proyectos',
+    tit_info_basica: 'Información Básica',
 
-    cmp_nombre: "Nombre",
-    cmp_objetivo: "Objetivo",
-    cmp_identificador: "Identificador",
-    cmp_descripcion: "Descripción",
-    cmp_cientifico_leader: "Cientifico Lider",
-    cmp_estado: "Estado",
-    cmp_fecha_creacion: "Fecha de Creación",
-    cmp_fecha_inicio: "Fecha de Inicio",
-    cmp_integrantes: "Integrantes",
-    acc_guardar: "Guardar",
-    acc_cancelar: "Cancelar"
-}
+    cmp_nombre: 'Nombre',
+    cmp_objetivo: 'Objetivo',
+    cmp_identificador: 'Identificador',
+    cmp_descripcion: 'Descripción',
+    cmp_cientifico_leader: 'Científico Líder',
+    cmp_estado: 'Estado',
+    cmp_fecha_creacion: 'Fecha de Creación',
+    cmp_fecha_inicio: 'Fecha de Inicio',
+    cmp_integrantes: 'Integrantes',
+    acc_guardar: 'Guardar',
+    acc_cancelar: 'Cancelar',
+
+    msg_no_exp_proyecto: 'No existen experimentos asociados al proyecto. Click acá para agregar.'
+};
 
 @Injectable()
 export class LabelsService {
