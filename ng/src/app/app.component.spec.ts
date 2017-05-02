1d28884b19c9f47a43cda2551b161f4df0f5d7c1
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-05-01 20:57:28 -0500

    Pruebas Unitarias en integracion continua

--- ng/src/app/app.component.spec.ts
+++ ng/src/app/app.component.spec.ts
@@ -1,31 +1,73 @@
 import {TestBed, async} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {LabModule} from './lab/lab.module';
+import {RouterTestingModule} from '@angular/router/testing';
+import {NavigationComponent} from './layout/navigation.component';
+import {TopNavBarComponent} from './layout/topnavbar.component';
+import {FooterComponent} from './layout/footer.component';
+import {SimpleNotificationsModule} from 'angular2-notifications/dist';
 
-describe('AppComponent', () => {
+describe('AppComponent Main Layout', () => {
+    let originalTimeout;
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                AppComponent
+                AppComponent,
+                FooterComponent,
+                NavigationComponent,
+                TopNavBarComponent,
             ],
+            imports: [
+                SimpleNotificationsModule.forRoot(),
+                RouterTestingModule,
+                LabModule ],
         }).compileComponents();
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     }));
 
-    it('should create the app', async(() => {
+    afterEach(function() {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+
+    it('debe crear app', async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     }));
 
-    it(`should have as title 'app works!'`, async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('app works!');
-    }));
-
-    it('should render title in a h1 tag', async(() => {
+    it('debe mostrar el menu', async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('app works!');
+        expect(compiled.querySelector('lab-navigation')).toBeTruthy();
     }));
+
+    it('debe mostrar el encabezado', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('lab-top-nav-bar')).toBeTruthy();
+    }));
+
+    it('debe mostrar el pie de pagina', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('lab-footer')).toBeTruthy();
+    }));
+    it('debe contener las notificaciones', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('simple-notifications')).toBeTruthy();
+    }));
+    it('debe contener el router', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('router-outlet')).toBeTruthy();
+    }));
+
 });
