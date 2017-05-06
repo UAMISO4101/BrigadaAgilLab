3f98646d7d499ba652d4f2998aebdaef1f29ce4d
alejandroquintero <alejandr0quinter0@yahoo.com>
2017-05-06 17:50:54 -0500

    se arregla scroll para detalle protocolo

--- ng/src/app/lab/experimento/experimento.asociar.protocolo.component.ts
+++ ng/src/app/lab/experimento/experimento.asociar.protocolo.component.ts
@@ -9,48 +9,54 @@ import {ActivatedRoute} from "@angular/router";
     selector: 'experimento-protocolo',
     moduleId: module.id,
     templateUrl: 'experimento.asociar.protocolo.component.html',
-    providers:[ProtocoloService,ExperimentoService]
+    providers: [ProtocoloService, ExperimentoService]
 })
-export class ExperimentoAsociarProtocoloComponent implements OnInit{
+export class ExperimentoAsociarProtocoloComponent implements OnInit {
 
- public protocolo:Protocolo[]=[];
- public protocolos:Protocolo[]=[];
- public idExperimento:string;
- public experimento:Experimentos;
-public show:string;
-  constructor(private _experimentoService:ExperimentoService,
-              private _protocoloService:ProtocoloService,
-              route: ActivatedRoute){
-this.idExperimento = route.snapshot.params['id'];
+    public protocolo: Protocolo[] = [];
+    public protocolos: Protocolo[] = [];
+    public idExperimento: string;
+    public experimento: Experimentos;
+    public show: string;
 
-  }
-onSelect(item){
-    this.protocolo = item;
-    this.show="true";
+    constructor(private _experimentoService: ExperimentoService,
+                private _protocoloService: ProtocoloService,
+                route: ActivatedRoute) {
+        this.idExperimento = route.snapshot.params['id'];
 
-}
- getProtocolos(){
-    this._protocoloService.listarProtocolos().subscribe((protocolos:Protocolo[])=>this.protocolos=protocolos);
-  }
-  getExperimento(){
-   this._experimentoService.getExperimentos().subscribe((experimento:Experimentos[])=>
-     this.experimento=experimento.filter(p=>p.id==this.idExperimento).pop(),
-   error=>console.log(error),
-     ()=>this.asociarProtocolo(this.experimento,this.protocolo))
-  }
-  asociarProtocolo(experimento,protocolo){
+    }
 
-    if(!experimento["protocolos"])
-      experimento["protocolos"]=[];
-    experimento["protocolos"].push(protocolo);
+    onSelect(item) {
+        this.protocolo = item;
+        this.show = "true";
 
-    this._experimentoService.asociarProtocolo(experimento,protocolo).subscribe(res=>console.log("success"),error=>console.log(error),
-        ()=>window.history.back());
+    }
 
-  }
+    getProtocolos() {
+        this._protocoloService.listarProtocolos().subscribe((protocolos: Protocolo[]) => this.protocolos = protocolos);
+    }
 
-  ngOnInit(){
-    this.getProtocolos();
-    window.scrollTo(0, 0);
-  }
+    getExperimento() {
+        this._experimentoService.getExperimentos().subscribe((experimento: Experimentos[]) =>
+                this.experimento = experimento.filter(p => p.id == this.idExperimento).pop(),
+            error => console.log(error),
+            () => this.asociarProtocolo(this.experimento, this.protocolo))
+    }
+
+    asociarProtocolo(experimento, protocolo) {
+
+        if (!experimento["protocolos"])
+            experimento["protocolos"] = [];
+        experimento["protocolos"].push(protocolo);
+
+        this._experimentoService.asociarProtocolo(experimento, protocolo)
+            .subscribe(res => console.log("success"), error => console.log(error),
+            () => window.history.back());
+
+    }
+
+    ngOnInit() {
+        this.getProtocolos();
+        window.scrollTo(0, 0);
+    }
 }
