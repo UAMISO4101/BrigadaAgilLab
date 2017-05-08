4103576e6de8bc01c325d88e26fef577eb86f1f1
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-05-07 19:33:36 -0500

    Ajustes de UI para ocultar el menu en dispositivos moviles

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -1,9 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, NgZone} from "@angular/core";
+
+declare var jQuery: any;
 
 @Component({
-    selector: 'lab-app',
-    templateUrl: 'app.component.html',
-    styleUrls: ['./app.component.css']
+    selector: "app-lab",
+    templateUrl: "app.component.html",
+    styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+
+    constructor(private ngZone: NgZone) {
+        this.menuSmallDisplay(window.innerWidth);
+        window.onresize = (e) => {
+            this.ngZone.run(() => {
+                this.menuSmallDisplay(window.innerWidth);
+            });
+        };
+    }
+
+    menuSmallDisplay(width: number) {
+        if (width < 769) {
+            jQuery("body").addClass("body-small");
+        } else {
+            jQuery("body").removeClass("body-small");
+        }
+    }
+
 }
